54ecf9690e99122ba67c9c38bc04f8eeda596a68
Satyaprakash Singh <107452937+Satya057@users.noreply.github.com>
2024-05-02 17:17:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('Timenews.json')
+    .then(response => response.json())
+    .then(data => {
+        const latestStoriesElement = document.getElementById("latest-stories");
+        data.forEach(story => {
+            const storyElement = document.createElement("div");
+            storyElement.classList.add("story");
+
+            const titleLink = document.createElement("a"); 
+            titleLink.classList.add("title-link"); 
+            titleLink.textContent = story.title; 
+            titleLink.href = story.link; 
+            titleLink.target = "_blank"; 
+
+            storyElement.appendChild(titleLink); 
+
+            latestStoriesElement.appendChild(storyElement); 
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
